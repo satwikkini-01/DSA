6b92e8908276ffdcc54d5a57323e4d528cd79407
Satwik Kini <144726232+satwikkini-01@users.noreply.github.com>
2023-10-19 09:23:08 +0530

    Time: 3 ms (47.11%) | Memory: 6.9 MB (15.59%) - LeetSync

---
+++ 874-backspace-string-compare/backspace-string-compare.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        stack<char>s1,s2;
+        for(int i=0;i<s.size();i++){
+            if(s1.empty() && s[i]=='#') continue;
+            if(s[i]=='#') s1.pop();
+            else s1.push(s[i]);
+        }
+        for(int i=0;i<t.size();i++){
+            if(s2.empty() && t[i]=='#') continue;
+            if(t[i]=='#') s2.pop();
+            else s2.push(t[i]);
+        }
+        while(!s1.empty()){
+            if(s2.empty()) return false;
+            if(s1.top()!=s2.top()) return false;
+            s1.pop();
+            s2.pop();
+        }
+        if(s1.empty() && s2.empty()) return true;
+        return false;
+    }
+};
