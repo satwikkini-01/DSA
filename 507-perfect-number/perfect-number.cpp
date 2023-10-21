6bcc0a5a961fbf0de34f85a4c5e1d821bf28eb16
Satwik Kini <144726232+satwikkini-01@users.noreply.github.com>
2023-10-21 11:40:12 +0530

    Time: 0 ms (100.00%) | Memory: 6.4 MB (9.71%) - LeetSync

--- 507-perfect-number/perfect-number.cpp
+++ 507-perfect-number/perfect-number.cpp
@@ -1,11 +1,7 @@
 class Solution {
 public:
-    bool checkPerfectNumber(int num) {
-        long long sum=0;
-        for(int i=1;i<num;i++){
-            if(num%i==0) sum+=i;
-        }
-        if(sum==num) return true;
+    bool checkPerfectNumber(int n) {
+        if(n==6 || n==28 || n==496 || n==8128 || n==33550336) return true;
         return false;
     }
 };
