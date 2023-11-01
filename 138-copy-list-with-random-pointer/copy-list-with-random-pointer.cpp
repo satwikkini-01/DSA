b9e09e0b18ff037b8c61d4fe5713c5286c23b264
Satwik Kini <144726232+satwikkini-01@users.noreply.github.com>
2023-11-01 18:55:16 +0530

    Time: 3 ms (95.90%) | Memory: 11.6 MB (17.70%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -28,25 +28,35 @@ public:
     }
 
     Node* copyRandomList(Node* head) {
-        unordered_map<Node*,Node*>mpp;
         Node* cloneHead = NULL, *cloneTail = NULL;
         Node* temp = head;
         while(temp!=NULL){
             insertAtTail(cloneHead,cloneTail,temp->val);
             temp=temp->next;
         }
-        Node* temp2 = cloneHead;
         temp = head;
+        Node* temp2 = cloneHead;
+        Node* prev1 = NULL,*prev2 = NULL;
         while(temp!=NULL){
-            mpp[temp] = temp2;
-            temp=temp->next;
-            temp2=temp2->next;
+            prev1 = temp->next;
+            temp->next = temp2;
+            prev2 = temp2->next;
+            temp2->next = prev1;
+            temp = prev1;
+            temp2 = prev2;
+        }
+        temp = head;
+        while(temp){
+            if(temp->random) temp->next->random = temp->random->next;
+            else temp->next->random = NULL;
+            temp = temp->next->next;
         }
         temp = head;
         temp2 = cloneHead;
-        while(temp!=NULL){
-            temp2->random = mpp[temp->random];
-            temp=temp->next;
+        while(temp!=NULL && temp2!=NULL){
+            temp->next = temp2->next;
+            temp = temp->next;
+            if(temp!=NULL) temp2->next = temp->next;
             temp2=temp2->next;
         }
         return cloneHead;
