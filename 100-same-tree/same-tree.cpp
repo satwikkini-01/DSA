9cea771a6247f3b970b876570115f13fec4742b0
Satwik Kini <144726232+satwikkini-01@users.noreply.github.com>
2024-02-06 17:29:33 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (23.42%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -11,28 +11,10 @@
  */
 class Solution {
 public:
-    void preOrder(TreeNode* root, vector<int>& ans){
-        if(root == NULL){
-            ans.push_back(-1);
-            return;
-        }
-        ans.push_back(root -> val);
-        preOrder(root -> left,ans);
-        preOrder(root -> right,ans);
-    }
-
-    vector<int> preorderTraversal(TreeNode* root) {
-        vector<int>ans;
-        preOrder(root, ans);
-        return ans;
-    }
-
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        vector<int> a = preorderTraversal(p);
-        vector<int> b = preorderTraversal(q);
-        for(int i=0;i<a.size();i++){
-            if(a[i] != b[i]) return false;
-        }
-        return true;
+        if(p == NULL && q == NULL) return true;
+        if(p == NULL || q == NULL) return false;
+        if(p->val==q->val) return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
+        return false;
     }
 };
