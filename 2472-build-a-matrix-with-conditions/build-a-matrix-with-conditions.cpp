4962bdd682e7791649bec55ba0ff329bd95dce0f
Satwik Kini <144726232+satwikkini-01@users.noreply.github.com>
2024-07-21 23:26:00 +0530

    Time: 107 ms (47.61%) | Memory: 68.1 MB (10.42%) - LeetSync

---
+++ 2472-build-a-matrix-with-conditions/build-a-matrix-with-conditions.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<int> khan(int k,vector<vector<int>>& Conditions){
+     vector<int>graph[k+1];
+     vector<int>degree(k+1);
+     for(auto i:Conditions){
+        graph[i[0]].push_back(i[1]);
+        degree[i[1]]++;
+     }
+     queue<int>q;
+     for(int i=1;i<k;i++){
+        if(degree[i]==0){
+            q.push(i);
+        }
+     }
+     vector<int>ans;
+     while(!q.empty()){
+         int t=q.front();
+         q.pop();
+         ans.push_back(t);
+         for(auto i:graph[t]){
+            degree[i]--;
+            if(degree[i]==0){
+                q.push(i);
+            }
+         }
+     }
+
+    return ans;
+    }
+    vector<vector<int>> buildMatrix(int k, vector<vector<int>>& rowConditions, vector<vector<int>>& colConditions) {
+        vector<int>row=khan(k+1,rowConditions);
+        vector<int>col=khan(k+1,colConditions);
+        vector<vector<int>>ans(k,vector<int>(k,0));
+        map<int ,int>mpp;
+        int n = row.size(), m = col.size();
+        if(n<k || m<k)return {};
+        for(int i=0;i<k;i++) mpp[col[i]]=i;
+        for(int i=0;i<k;i++){
+            int col_pos=mpp[row[i]];
+            ans[i][col_pos]=row[i];
+        }
+        return ans;
+    }
+};
