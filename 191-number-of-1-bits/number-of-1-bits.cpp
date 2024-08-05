1f38b5606053703a50aa65406d574b71da5fcc1d
Satwik Kini <144726232+satwikkini-01@users.noreply.github.com>
2024-08-05 23:35:54 +0530

    Time: 2 ms (37.47%) | Memory: 8.1 MB (15.38%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.cpp
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
-    int hammingWeight(uint32_t n) {
-        int ans=0;
-        while(n>0){
-            n &= (n-1);
-            ans++;
+    int hammingWeight(int n) {
+        int b[32];
+        int i = 0, ans = 0;
+        while (n > 0) {
+            b[i] = n % 2;
+            n = n / 2;
+            i++;
         }
+        for (int j=i-1;j>=0;j--) if(b[j] == 1) ans++;
         return ans;
     }
 };
