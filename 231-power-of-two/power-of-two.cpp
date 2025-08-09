6d8f9ce2f9367da88f4e7c687123066c42138914
Satwik Kini <144726232+satwikkini-01@users.noreply.github.com>
2025-08-10 01:04:53 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (52.81%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n<=0) return false;
-        while(n>0){
-            if(2%n!=0 && n%2!=0) return false;
-            n/=2;
-        }
-        return true;
+        if(n <= 0) return false;
+        if(n == 1) return true;
+        if(n%2 != 0) return false;
+        return isPowerOfTwo(n/2);
     }
 };
