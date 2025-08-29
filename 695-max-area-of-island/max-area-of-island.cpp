52bc5609a3cfda8386b4f26b417a6fceb9ce1c5b
Satwik Kini <144726232+satwikkini-01@users.noreply.github.com>
2025-08-29 19:25:45 +0530

    Time: 11 ms (15.14%) | Memory: 30.5 MB (28.57%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.cpp
+++ 695-max-area-of-island/max-area-of-island.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& grid, int i, int j, int n, int m, int& ans){
-        grid[i][j] = 0;
-        ans++;
-        if(i-1>=0 && grid[i-1][j] == 1) dfs(grid, i-1, j, n ,m, ans);
-        if(j-1>=0 && grid[i][j-1] == 1) dfs(grid, i, j-1, n, m, ans);
-        if(i+1<n && grid[i+1][j] == 1) dfs(grid, i+1, j, n, m, ans);
-        if(j+1<m && grid[i][j+1] == 1) dfs(grid, i, j+1, n, m, ans);
+    void dfs(vector<vector<int>>& grid, vector<vector<bool>>& vis, int i, int j, int& cnt){
+        vis[i][j] = true;
+        cnt++;
+        vector<pair<int, int>> dirs = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+        for(auto [di, dj]: dirs){
+            int ni = i+di, nj = j+dj;
+            if(ni>=0 && nj>=0 && ni<grid.size() && nj<grid[0].size() && !vis[ni][nj] && grid[ni][nj]){
+                dfs(grid, vis, ni, nj, cnt);
+            }
+        }
     }
 
     int maxAreaOfIsland(vector<vector<int>>& grid) {
         int n = grid.size(), m = grid[0].size(), ans = 0;
+        vector<vector<bool>> vis(n, vector<bool>(m, false));
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j] == 1){
+                if(grid[i][j] && !vis[i][j]){
                     int tmp = 0;
-                    dfs(grid, i, j, n, m, tmp);
-                    ans = max(tmp, ans);
+                    dfs(grid, vis, i, j, tmp);
+                    ans = max(ans, tmp);
                 }
             }
         }
