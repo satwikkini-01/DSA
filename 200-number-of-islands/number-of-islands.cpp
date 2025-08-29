69dac7cfdd9ee170f1b18edf0d7d69becdbfafe4
Satwik Kini <144726232+satwikkini-01@users.noreply.github.com>
2025-08-29 19:18:38 +0530

    Time: 41 ms (13.16%) | Memory: 23.3 MB (34.33%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
-    void dfs(vector<vector<char>>& grid, int i, int j, int n, int m){
-        grid[i][j] = 0;
-        if(i-1>=0 && grid[i-1][j] == '1') dfs(grid, i-1, j, n, m);
-        if(j-1>=0 && grid[i][j-1] == '1') dfs(grid, i, j-1, n, m);
-        if(i+1<n && grid[i+1][j] == '1') dfs(grid, i+1, j, n, m);
-        if(j+1<m && grid[i][j+1] == '1') dfs(grid, i, j+1, n, m); 
+    void dfs(vector<vector<char>>& grid, int i, int j, vector<vector<int>>& vis){
+        vis[i][j] = true;
+        vector<pair<int, int>> dirs = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+        for(auto [di, dj]: dirs){
+            int ni = i+di, nj = j+dj;
+            if(ni>=0 && nj>=0 && ni<grid.size() && nj<grid[0].size() && !vis[ni][nj] && grid[ni][nj] == '1')
+            dfs(grid, ni, nj, vis);
+        }
     }
 
     int numIslands(vector<vector<char>>& grid) {
         int n = grid.size(), m = grid[0].size(), ans = 0;
+        vector<vector<int>> vis(n, vector<int>(m, false));
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j] == '1'){
+                if(grid[i][j] == '1' && !vis[i][j]){
+                    dfs(grid, i, j, vis);
                     ans++;
-                    dfs(grid, i, j, n, m);
                 }
             }
         }
