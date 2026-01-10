ccc8aa2d792ce1c0d01630cc90a8a9a52dc8d708
Satwik Kini <144726232+satwikkini-01@users.noreply.github.com>
2026-01-10 19:46:54 +0530

    Time: 3 ms (47.82%) | Memory: 9.9 MB (25.44%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if(s == t) return true;
-        return false;
+        if(s.size() != t.size()) return false;
+        map<char, int>m1, m2;
+        for(auto it:s) m1[it]++;
+        for(auto it:t) m2[it]++;
+        for(auto it:m1){
+            if(m2[it.first] != it.second) return false;
+        }
+        return true;
     }
 };
